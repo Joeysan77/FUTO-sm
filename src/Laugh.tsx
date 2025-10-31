b7d842455b20beb7d930d41bafb6db2db73ab32b
Joeysan77 <joeysandesh@gmail.com>
2025-10-31 18:59:27 +0100

    Fix type errors and UI

--- src/Laugh.tsx
+++ src/Laugh.tsx
@@ -1,7 +1,15 @@
-export function Laugh({show}) {
-    return (
-        <div className={`bg-white fixed z-10 top-0 lef-0 w-full h-full flex ${show ? "show" : "hide"}`}>
-            <img src="/laugh.gif" className="w-full mt-29"alt=""/>
-        </div>
-    )
+interface LaughProps {
+  show: boolean
+}
+
+export function Laugh({ show }: LaughProps) {
+  return (
+    <div
+      className={`bg-white fixed z-10 top-0 left-0 w-full h-full flex ${
+        show ? "show" : "hide"
+      }`}
+    >
+      <img src="/laugh.gif" className="w-full mt-29" alt="laugh animation" />
+    </div>
+  )
 }
--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
     return (
         <div className="py-10 px-15 mb-5">
-            <img src="/logoo.png" class alt=""/>
+            <img src="/logoo.png" alt=""/>
         </div>
     )
 }
